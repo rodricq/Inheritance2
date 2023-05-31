0b783e654b01b28b470a291362350539d42a033f
beverley-dick <beverleydick11@gmail.com>
2023-05-31 09:42:45 +0300

    Add project files.

---
+++ Inheritance2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Inheritance2
+{
+    //base class
+    class Animal
+    {
+        protected void eat()
+        {
+            Console.WriteLine("I can eat");
+        }
+    }
+
+    // derived class of Animal
+    class Dog : Animal
+    {
+        static void Main(string[] args)
+        {
+            Dog labrador = new Dog();
+
+            //accessing protected method from base class
+            labrador.eat();
+
+            Console.ReadLine();
+        }
+    }
+}
